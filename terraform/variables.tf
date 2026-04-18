cadeff36498c2030f945f82bc6e07c200dfa3c4c
ludovite <18051237+ludovite@users.noreply.github.com>
2026-04-18 09:21:42 +0200

    Setup secrets & project variables in env vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,19 @@
 variable "project" {
-  description = "Project"
-  type = string
-  default     = "global-sea-survey"
+  description = "GCP project ID (set via TF_VAR_project / PROJECT_NAME)"
+  type        = string
 }
 
 variable "region" {
-  description = "Region"
-  type = string
-  default     = "europe-west9" # Paris, low CO₂
+  description = "GCP region (set via TF_VAR_region / GCP_LOCATION)"
+  type        = string
 }
 
 variable "gcs_bucket_name" {
-  description = "Storage Bucket Name"
-  type = string
-  default     = "global-sea-survey-bucket"
+  description = "GCS bucket name (set via TF_VAR_gcs_bucket_name / GCP_BUCKET)"
+  type        = string
 }
 
 variable "bq_dataset_name" {
-  description = "BigQuery Dataset Name"
-  type = string
-  default     = "global_sea_survey_bq_dataset"
+  description = "BigQuery dataset ID (set via TF_VAR_bq_dataset_name / GCP_DATASET)"
+  type        = string
 }
